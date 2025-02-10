99f70ae4a60aa5e374163147f3cd8a5054ffa6dd
Compuute <160307775+Compuute@users.noreply.github.com>
2025-02-10 11:29:09 +0100

    Added inmatnings formulär

--- user.js
+++ user.js
@@ -1,9 +1,11 @@
-export class User{
-    constructor(email, caption, description){
-        this.email = email;
-        this.caption = caption;
-        this.description = description;
-        this.issueDate = Date.now();
-        this.issuNumber = 1;
-    }
+
+
+export class User {
+  constructor(email, caption, description) {
+    this.email = email;
+    this.caption = caption;
+    this.description = description;
+    this.issueDate = new Date().toLocaleDateString("sv-SE");
+    this.issuNumber = crypto.randomUUID().replaceAll("-", "");
+  }
 }
